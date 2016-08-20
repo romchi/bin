587f049ca1ef6f6b701cdc40834a16249b4673e1
Roman Bulakh <bulah.roman@gmail.com>
2016-08-20 22:39:18 +0300

    rebase

---
+++ synclient.sh
@@ -0,0 +1,86 @@
+#
+# Configuration synclient
+#
+
+synclient TouchpadOff=0
+synclient PalmDetect=0
+synclient PalmMinWidth=10
+synclient PalmMinZ=150
+
+synclient ClickFinger1=1
+synclient ClickFinger2=3
+synclient ClickFinger3=2
+
+synclient LockedDrags=1
+synclient LockedDragTimeout=5000
+synclient RTCornerButton=0
+synclient RBCornerButton=0
+synclient LTCornerButton=0
+synclient LBCornerButton=0
+synclient TapButton1=1
+synclient TapButton2=3
+synclient TapButton3=2
+
+synclient LeftEdge=1000
+synclient RightEdge=4000
+synclient TopEdge=1000
+synclient BottomEdge=4000
+
+synclient FingerLow=40
+synclient FingerHigh=45
+
+synclient MaxTapTime=180
+synclient MaxTapMove=242
+synclient MaxDoubleTapTime=180
+synclient SingleTapTimeout=180
+synclient ClickTime=100
+
+synclient ClickPad=1
+
+synclient VertEdgeScroll=0
+synclient HorizEdgeScroll=0
+synclient CornerCoasting=0
+synclient VertTwoFingerScroll=1
+synclient HorizTwoFingerScroll=1
+synclient VertScrollDelta=99
+synclient HorizScrollDelta=99
+synclient CircularScrolling=1
+synclient CircScrollDelta=0.5
+synclient CircScrollTrigger=2
+synclient CircularPad=1
+
+synclient MinSpeed=1
+synclient MaxSpeed=2
+synclient AccelFactor=0.05
+
+synclient PressureMotionMinZ=30
+synclient PressureMotionMaxZ=160
+synclient PressureMotionMinFactor=1
+synclient PressureMotionMaxFactor=1
+
+synclient HorizHysteresis=5
+synclient VertHysteresis=5
+
+synclient EmulateMidButtonTime=0
+
+synclient EmulateTwoFingerMinZ=250
+synclient EmulateTwoFingerMinW=5
+
+synclient CoastingSpeed=20
+synclient CoastingFriction=50
+synclient SingleTapTimeout=150
+synclient TapAndDragGesture=1
+
+synclient AreaLeftEdge=0
+synclient AreaRightEdge=0
+synclient AreaTopEdge=0
+synclient AreaBottomEdge=0
+
+synclient RightButtonAreaLeft=3292
+synclient RightButtonAreaRight=0
+synclient RightButtonAreaTop=3397
+synclient RightButtonAreaBottom=0
+synclient MiddleButtonAreaLeft=0
+synclient MiddleButtonAreaRight=0
+synclient MiddleButtonAreaTop=0
+synclient MiddleButtonAreaBottom=0
