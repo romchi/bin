a486df26dc95eb9b7d45a35529d8382adbeac784
Roman Bulakh <bulah.roman@gmail.com>
2019-01-07 22:09:23 +0200

    Add alias and features

---
+++ vim/templates/=template=.go
@@ -0,0 +1,15 @@
+//
+// =template=.go
+//
+// Copyright (C) 2018 Roman Bulakh <bulah.roman@gmail.com>
+//
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("vim-go")
+}
--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -3,4 +3,6 @@ alias ssh='TERM=rxvt ssh'
 alias etoken-reload='source ~/bin/reload.sh'
 alias vi='vim'
 alias tmux='tmux -2'
-alias zshconfig="vim ~/.zshrc"
+alias zshconfig='vim ~/.zshrc'
+
+alias topmem='ps -A --sort -rss -o comm,pmem,rss | head -n 20'
